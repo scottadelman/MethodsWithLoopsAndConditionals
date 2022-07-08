808edcd0051be7f0426d40f1fa6c9f8c1128c2e7
Scott <ScottAdelman97@gmail.com>
2022-07-08 13:43:33 -0600

    heres some methods!

---
+++ MethodsWithLoopsAndConditionals/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsWithLoopsAndConditionals
+{
+    internal class Class1
+    {
+        public static void Method1()
+        {
+            for (int i = -1000; i >= -999 && i <= 1000; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            int x = 1000;
+            while (x >= -1000)
+            {
+                Console.WriteLine(x);
+                x--;
+            }
+
+        }
+        public static void Method2()
+        {
+            for (int i = 0; i <= 999; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public static void Method3() //guess my favorite color
+        {
+            Console.WriteLine("Guess my favorite color");
+            string myFavColor = "purple";
+            bool didYouGuessIt;
+
+            while (didYouGuessIt = true)
+            {
+                if (Console.ReadLine() != myFavColor)
+                {
+                    Console.WriteLine("Nope, guess again");
+                    didYouGuessIt = false;
+                }
+                else
+                {
+                    Console.WriteLine("You got it!");
+                    break;
+                }
+            }
+        }
+        public static void Method4()
+        {
+            Console.WriteLine("Let's check if your number is even!");
+            bool isItEven;
+
+            while(isItEven = true || false)
+            {
+                int x = int.Parse(Console.ReadLine());
+                
+                if (x % 2 == 0)
+                {
+                    Console.WriteLine("This is an even number!");
+                }
+                else
+                {
+                    Console.WriteLine("This is an odd number!");
+                }
+            }
+
+        }
+    }
+}
---
+++ MethodsWithLoopsAndConditionals/Program.cs
@@ -0,0 +1,6 @@
+﻿using MethodsWithLoopsAndConditionals;
+
+//Class1.Method1();
+//Class1.Method2();
+//Class1.Method3();
+Class1.Method4();   
